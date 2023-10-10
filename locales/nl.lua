0c68b7b4d0e72451037e9c9961938d9992025556
MaDHouSe79 <madhouse1979@gmail.com>
2023-10-10 09:06:46 +0200

    Update nl.lua

--- locales/nl.lua
+++ locales/nl.lua
@@ -5,6 +5,7 @@ local Translations = {
         ['not_the_right_items'] = "Je hebt niet de juiste items..",
         ['no_cash'] = "Je hebt niet genoeg contant geld..",
         ['can_not_sell_item'] = "Je kunt dit item niet verkopen..",
+        ['can_not_open'] = "Je kunt deze kofferbak niet openen!",
     },
 }
 
